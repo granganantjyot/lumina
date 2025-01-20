eb8edb173fadb1c75c42956a65e9ba7e7885253a
Anantjyot Grang <granganantjyot@gmail.com>
2025-01-20 17:56:31 -0500

    Updated global font to Geist

--- frontend/lumina/tailwind.config.ts
+++ frontend/lumina/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 export default {
     darkMode: ["class"],
@@ -55,7 +56,10 @@ export default {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+		fontFamily: {
+			sans: ["var(--font-geist-sans)", ...fontFamily.sans], // Use Geist Variable for the sans font
+		},
   	}
   },
   plugins: [require("tailwindcss-animate")],
