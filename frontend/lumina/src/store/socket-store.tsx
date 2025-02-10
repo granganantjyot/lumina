05a185c135b8d8984fc97548d8e139c03102ba2a
Anantjyot Grang <granganantjyot@gmail.com>
2025-02-09 21:46:04 -0500

    Created zustand store for websocket state management

---
+++ frontend/lumina/src/store/socket-store.tsx
@@ -0,0 +1,94 @@
+import { ImageFrame } from "@/components/photo-crop";
+import { create } from "zustand";
+
+
+interface ImageSocketState {
+    socket: WebSocket | null // Websocket
+
+    activePreviews: {[parentImageID: string] : string[]} // Map parent img ID, to array of base64 string images
+
+    connect: () => void // Used to make initial websocket connection
+
+    requestPreviewUpdate: (parentImageID: string, index: number, newFrame: ImageFrame, stageScale: {xScale: number, yScale: number}) => void // Send message to socket
+
+    resetPreviews: (parentImageID: string, stageScale: {xScale: number, yScale: number}, oldFrames: ImageFrame[]) => void 
+
+    disconnect: () => void
+
+}
+
+export const useImageSocketStore = create<ImageSocketState>()((set, get) => ({
+
+    socket: null,
+
+    activePreviews: {},
+
+    connect: () => {
+        console.log("Connecting to socket...")
+        const imageSocket = new WebSocket("ws://localhost:8765/");
+        
+
+        imageSocket.onmessage = (event: MessageEvent) => {
+            const response = JSON.parse(event.data);
+
+            set((state) => {
+                // Get current previews for the specific parentImageID
+                const currentPreviews = state.activePreviews[response.parentImageID] || [];
+                
+
+                // Create copy of these previews, and update at the relevant index
+                const updatedPreviews = [...currentPreviews]; 
+                updatedPreviews[response.index] = response.image; 
+
+                // Update store state for active previews
+                return {activePreviews: {...state.activePreviews, [response.parentImageID] : updatedPreviews}}
+            })
+        }
+
+        set({socket : imageSocket});
+    },
+
+    requestPreviewUpdate: (parentImageID: string, index: number, newFrame: ImageFrame, stageScale) => {
+        // Create deep copy of updatedFrame 
+        const rescaledFrame: ImageFrame = {
+            tl: [...newFrame.tl],
+            tr: [...newFrame.tr],
+            br: [...newFrame.br],
+            bl: [...newFrame.bl]
+        };
+
+        // Divide by x and y scale factors for each corner in the ImageFrame (to get it into original pixel-scale of the image)
+        Object.keys(rescaledFrame).forEach((key) => {
+            const [x, y] = rescaledFrame[key as keyof ImageFrame];
+            rescaledFrame[key as keyof ImageFrame] = [x / stageScale.xScale, y / stageScale.yScale]
+        })
+
+        const body = {
+            parentImageID: parentImageID,
+            index: index,
+            frame: rescaledFrame
+        }
+
+        get().socket?.send(JSON.stringify(body));
+    },
+
+    resetPreviews: (parentImageID: string, stageScale: {xScale: number, yScale: number}, oldFrames: ImageFrame[]) => {
+
+        set((state) => ({activePreviews: {...state.activePreviews, [parentImageID] : []}}))
+
+        const requestPreviewUpdate = get().requestPreviewUpdate;
+
+        for (let i = 0; i < oldFrames.length; i++){
+            requestPreviewUpdate(parentImageID, i, oldFrames[i], stageScale)
+        }
+    },
+        
+    
+
+    disconnect: () => {
+        get().socket?.close();
+
+        set({socket: null});
+    }
+
+}))
