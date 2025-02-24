e3b1b50f4b151b3ccffc8be7baa8012dfd3d965b
Anantjyot Grang <granganantjyot@gmail.com>
2025-02-23 21:01:53 -0500

    Created store for confirmed images

---
+++ frontend/lumina/src/store/confirmed-image-store.tsx
@@ -0,0 +1,27 @@
+import { create } from 'zustand'
+
+
+export interface ConfirmedImage {
+    imageID: string,
+    angle: number,
+    date: string,
+    parentImageID: string
+}
+
+
+interface ConfirmedImageState {
+    confirmedImages: ConfirmedImage[] | null,
+
+    setConfirmedImages : (newImages : ConfirmedImage[]) => void;
+}
+
+
+const useConfirmedImageStore = create<ConfirmedImageState>((set) => ({
+
+    confirmedImages: null,
+
+    setConfirmedImages: (newImages : ConfirmedImage[]) => set((state) => ({confirmedImages: newImages}))
+
+}))
+
+export default useConfirmedImageStore;
