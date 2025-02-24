4580d7252390ceb68cb3ebd836dd7638dd1313c5
Anantjyot Grang <granganantjyot@gmail.com>
2025-02-23 21:00:48 -0500

    Restructured request body for process endpoint

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,11 +1,20 @@
+import json
+from textwrap import indent
 from fastapi import UploadFile, APIRouter, Form, Request
+from fastapi.responses import FileResponse
 from typing import Annotated, Dict
 from processing.corners import get_images_corners
 import os
 import shutil
 from processing.enhance import enhance_parallel
+from processing.manual_rotate import rotate_processed_image
+from processing.update_metadata import apply_date_metadata
+import shutil
 
 router = APIRouter()
+UPLOAD_FOLDER = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "../uploads")
+
 
 @router.post("/api/upload")
 async def upload(files: list[UploadFile], session_id: Annotated[str, Form()],):
@@ -13,45 +22,39 @@ async def upload(files: list[UploadFile], session_id: Annotated[str, Form()],):
     result = []
     count = 0
 
-
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../uploads")
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
 
     for file in files:
 
         # Iterate through all files, and get all image frames
         images_corners = await get_images_corners(file)
-        img_code = session_id + "_" + str(count) # use this image code to store the actual image in session storage
-        result.append({"parentImgID" : img_code, "imageFrames" : images_corners})
-
+        # use this image code to store the actual image in session storage
+        img_code = session_id + "_" + str(count)
+        result.append({"parentImgID": img_code, "imageFrames": images_corners})
 
         # Save the file to uploads
-        file.file.seek(0)  
+        file.file.seek(0)
         file_path = os.path.join(UPLOAD_FOLDER, f"{img_code}.png")
         with open(file_path, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
 
-
         count += 1
-    
-    return {"filenames": [file.filename for file in files], "processedResult" : result}
+
+    return {"filenames": [file.filename for file in files], "processedResult": result}
 
 
 @router.post("/api/process")
-async def fix(request: Request):
+async def process(request: Request):
 
-
-    body = await request.json() # Contains parent image ids
+    body = await request.json()
+    parentImgToFrames = body['parentImgToFrames']  # Contains parent image ids
+    sessionId = body['sessionId']
 
     image_data = []
-    
 
-    for parent_img_id, child_images in body.items():
+    for parent_img_id, child_images in parentImgToFrames.items():
         for child_corners in child_images:
-            image_data.append((parent_img_id, child_corners))
+            image_data.append((parent_img_id, child_corners, sessionId))
 
-    
-        
+    return {"images": enhance_parallel(image_data)}
 
-    return {"images" : enhance_parallel(image_data)}
