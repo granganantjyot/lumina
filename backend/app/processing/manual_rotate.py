0c60352f8ed8c6fa98dbf45706d0696ec91244ed
Anantjyot Grang <granganantjyot@gmail.com>
2025-02-23 21:03:48 -0500

    Added manual rotation for processed images

---
+++ backend/app/processing/manual_rotate.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+
+def rotate_processed_image(session_id, image_id, angle):
+    if angle == 0: return
+
+    image_path = f"../processed_images/{session_id}/{image_id}.png"
+
+    image = cv2.imread(image_path)
+    
+    if angle == 90:
+        image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+    elif angle == 180:
+        image = cv2.rotate(image, cv2.ROTATE_180)
+    elif angle == 270:
+        image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+    cv2.imwrite(image_path, image)
+
+
+    
+
+    
