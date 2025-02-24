f616a6ee094776eb9731740ff75add0f309ca16c
Anantjyot Grang <granganantjyot@gmail.com>
2025-02-23 21:03:24 -0500

    Mounting downloads directory

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, File, UploadFile, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from routes import router
+import os
 
 
 app = FastAPI()
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,6 +17,12 @@ app.add_middleware(
 )
 
 app.include_router(router)
+
+
+# Mount downloads folder
+DOWNLOADS_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../downloads")
+os.makedirs(DOWNLOADS_FOLDER, exist_ok=True)
+app.mount("/downloads", StaticFiles(directory=DOWNLOADS_FOLDER), name="downloads")
 
 
 @app.get("/")
