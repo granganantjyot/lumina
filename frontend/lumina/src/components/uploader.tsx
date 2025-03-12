ca836cd7f4fb015a24ebacde55688140f3d3dba0
Anantjyot Grang <granganantjyot@gmail.com>
2025-03-12 13:01:23 -0400

    Allow .heic file uploads

--- frontend/lumina/src/components/uploader.tsx
+++ frontend/lumina/src/components/uploader.tsx
@@ -23,7 +23,7 @@ export default function UploadComponent({ className, onFileUpload }: UploadCompo
             }}
 
             maxSize={50 * 1024 * 1024} // 50mb max upload size
-            accept={{ "image/png": [], "image/jpeg": [], "image/jpg": [] }}
+            accept={{ "image/png": [], "image/jpeg": [], "image/jpg": [], "image/heic": [] }}
         >
             {(dropzone: DropzoneState) => (
                 <>
@@ -34,7 +34,7 @@ export default function UploadComponent({ className, onFileUpload }: UploadCompo
                                 <div className='flex items-center flex-col gap-1.5'>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-upload mr-2 h-4 w-4"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="17 8 12 3 7 8"></polyline><line x1="12" x2="12" y1="3" y2="15"></line></svg>
                                     <p className="text-sm font-medium text-center">Drop files or click to browse</p>
-                                    <p className="text-xs font-medium text-center">.png, .jpg, .jpg (50 MB max)</p>
+                                    <p className="text-xs font-medium text-center">.png, .jpg, .jpg, .heic (50 MB max)</p>
                                     <p className="text-xs text-center">{dropzone.acceptedFiles.length} file(s) uploaded</p>
                                 </div>
                             )
