6c461799b641be86f8495e3e49bc21b6c67fc134
Anantjyot Grang <granganantjyot@gmail.com>
2025-03-12 14:21:38 -0400

    Added max 5 files and display upload file names

--- frontend/lumina/src/components/uploader.tsx
+++ frontend/lumina/src/components/uploader.tsx
@@ -12,17 +12,23 @@ interface UploadComponentProps {
 export default function UploadComponent({ className, onFileUpload }: UploadComponentProps) {
     return (
         <Dropzone
-            dropZoneClassName="flex justify-center items-center w-full h-full bg-white border-dashed border-[1.5px] border-[#31454e] rounded-lg hover:bg-transparent hover:text-white hover:border-white transition-all select-none cursor-pointer"
+            dropZoneClassName="flex justify-center items-center w-full h-full bg-white border-solid border-[1px] border-white rounded-lg hover:bg-transparent hover:text-white hover:border-white transition-all select-none cursor-pointer"
             containerClassName={clsx("w-1/2 min-h-56", className)}
             onDrop={(acceptedFiles: File[]) => {
                 onFileUpload(acceptedFiles)
             }}
 
             onDropRejected={(fileRejections) => {
-                toast({title : "Unsupported File(s)", variant: "destructive",})
+                // Check if rejection cause was too many files
+                const tooManyFilesError = fileRejections.some(rejection => rejection.errors.some(error => error.code === "too-many-files"));
+
+                // Show toast based on rejection cause
+                if (tooManyFilesError) toast({title : "Maximum 5 Files", variant: "destructive"})
+                else toast({title : "Unsupported File(s)", variant: "destructive",})
             }}
 
             maxSize={50 * 1024 * 1024} // 50mb max upload size
+            maxFiles={5}
             accept={{ "image/png": [], "image/jpeg": [], "image/jpg": [], "image/heic": [] }}
         >
             {(dropzone: DropzoneState) => (
@@ -31,11 +37,11 @@ export default function UploadComponent({ className, onFileUpload }: UploadCompo
 
                         dropzone.isDragActive ? (<p>Ready!</p>)
                             : (
-                                <div className='flex items-center flex-col gap-1.5'>
+                                <div className='flex items-center flex-col gap-1.5  px-6'>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-upload mr-2 h-4 w-4"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="17 8 12 3 7 8"></polyline><line x1="12" x2="12" y1="3" y2="15"></line></svg>
-                                    <p className="text-sm font-medium text-center">Drop files or click to browse</p>
-                                    <p className="text-xs font-medium text-center">.png, .jpg, .jpg, .heic (50 MB max)</p>
-                                    <p className="text-xs text-center">{dropzone.acceptedFiles.length} file(s) uploaded</p>
+                                    <p className="text-sm font-medium text-center">Drop Files or Click to Browse</p>
+                                    <p className="text-xs font-medium text-center">png, jpg, jpg, heic (50 MB Max)</p>
+                                    <p className="text-xs text-center">{dropzone.acceptedFiles.map((file) => file.name).join(", ")}</p>
                                 </div>
                             )
 
