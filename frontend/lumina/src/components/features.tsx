2aca59351c9cb86a22693e9b5f541b325a2d7927
Anantjyot Grang <granganantjyot@gmail.com>
2025-03-17 14:34:42 -0400

    Using tailwind colors instead of hex values

--- frontend/lumina/src/components/features.tsx
+++ frontend/lumina/src/components/features.tsx
@@ -15,25 +15,25 @@ const features = [
     { 
         "name": "Multi-Photo Extraction", 
         "description": "Quickly extract multiple photos from a single scan or snapshot", 
-        "color": "#4cacaf",
+        "color": "main-teal",
         "icon" :  <Images size={iconSize}/>
     },
     { 
         "name": "Auto Image Enhancing", 
         "description": "Extracted photos are enhanced for improved quality and clarity", 
-        "color": "#e96443",
+        "color": "main-orange",
         "icon" :  <Sparkles size={iconSize}/>
     },
     { 
         "name": "AI Upscaling", 
         "description": "Upscale photos by 2x for higher resolution using deep learning", 
-        "color": "#c94b4b",
+        "color": "main-red",
         "icon" :  <ImageUpscale size={iconSize}/>
     },
     { 
         "name": "Metadata Integration", 
         "description": "Keep your photos chronologically organized with editable metadata", 
-        "color": "#31454e",
+        "color": "main-dark",
         "icon" :  <FolderCog size={iconSize}/>
     },
 
@@ -48,10 +48,10 @@ export default function FeaturesComponent({ className }: FeaturesComponentProps)
             {features.map((f, index) => (
                 <Card key={index} className="flex flex-col flex-1 min-w-[200px] lg:min-h-[300px] shadow-lg">
                     <CardContent className="flex flex-col items-center justify-center p-6 text-center h-full">
-                        <div className="mb-5" style={{ color: f.color }}>
+                        <div className={`mb-5 text-${f.color}`}>
                             {f.icon}
                         </div>
-                        <p className="text-2xl font-semibold" style={{ color: f.color }}>{f.name}</p>
+                        <p className={`text-2xl font-semibold text-${f.color}`}>{f.name}</p>
                         <p className="mt-1">{f.description}</p>
                     </CardContent>
                 </Card>
