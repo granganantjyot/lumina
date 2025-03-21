0411e630693d44b3439d69913b491a638b3c5d77
Anantjyot Grang <granganantjyot@gmail.com>
2025-03-21 18:16:32 -0400

    Implemented global queue for multiple preview requests

--- backend/app/websocket.py
+++ backend/app/websocket.py
@@ -7,6 +7,37 @@ from dotenv import load_dotenv
 import os
 
 
+load_dotenv()
+
+# Store preview requests in a queue (FIFO) to prevent overload
+global_queue = asyncio.Queue()
+
+
+async def process_messages():
+    while True:
+        websocket, body = await global_queue.get()
+
+        try:
+            print(f"executing {body["index"]} of {body["parentImageID"]}")
+
+            # Generate preview 
+            res = generate_preview(body["parentImageID"], body["frame"])
+
+            response = {
+                "image": f"data:image/jpg;base64,{res}",
+                "parentImageID": body["parentImageID"],
+                "index": body["index"]
+            }
+
+
+            await websocket.send(json.dumps(response))
+            print("Sent response")
+        except Exception as e:
+            print(f"Error processing message: {e}")
+        finally:
+            global_queue.task_done()  # Mark task as complete
+
+
 async def socket(websocket):
 
     # Do not allow connections from unauthorized origin
@@ -16,22 +47,15 @@ async def socket(websocket):
         print(f"Blocked WebSocket connection from unauthorized origin: {connection_origin}")
         await websocket.close()
         return
-    
-    
+
     # Process message
     try:
         while True:
             body = await websocket.recv()
             body = json.loads(body)
-            print(f'****Received: index {body['index']}')
 
-            res = generate_preview(body["parentImageID"], body["frame"])
+            await global_queue.put((websocket, body))
 
-            response = {"image": f"data:image/jpg;base64,{res}",
-                        "parentImageID": body["parentImageID"],
-                        "index": body["index"]}
-
-            await websocket.send(json.dumps(response))
             print("sent")
 
     except (ConnectionClosedOK, ConnectionClosedError) as e:
@@ -45,11 +69,11 @@ async def socket(websocket):
 
 
 async def main():
+    asyncio.create_task(process_messages())
     async with serve(socket, "127.0.0.1", os.getenv("WS_PORT")):
         print("Starting socket...")
-        await asyncio.get_running_loop().create_future()  # run forever
+        await asyncio.Future()  # run forever
 
 
 if __name__ == "__main__":
-    load_dotenv()
     asyncio.run(main())
