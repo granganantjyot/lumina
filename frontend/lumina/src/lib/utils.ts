f668118c6ff904ed170bdf6c10a6f3020eefa9b6
Anantjyot Grang <granganantjyot@gmail.com>
2025-03-23 22:15:57 -0400

    Created util function to convert base64 image string to blob object

--- frontend/lumina/src/lib/utils.ts
+++ frontend/lumina/src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function base64ImageToBlob(base64Value: string) {
+
+  const [prefix, rawBase64] = base64Value.split(',');
+  const mimeType = prefix.match(/:(.*?);/)?.[1] || "image/png";
+  const byteCharacters = atob(rawBase64);
+  const byteNumbers = new Array(byteCharacters.length).fill(0).map((_, i) => byteCharacters.charCodeAt(i));
+  const byteArray = new Uint8Array(byteNumbers);
+
+  return new Blob([byteArray], { type: mimeType });
+}
