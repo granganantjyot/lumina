c8857d48d85acd4cf920da543db8df8de6f7f522
Anantjyot Grang <granganantjyot@gmail.com>
2025-04-17 18:35:55 -0400

    Updating EXIF metadata with timestamp

--- backend/app/processing/update_metadata.py
+++ backend/app/processing/update_metadata.py
@@ -1,16 +1,39 @@
 import os
 import time
 from datetime import datetime
+from exif import Image
 
 
 def apply_date_metadata(session_id, image_id, date_string):
-    print(date_string)
-    image_path = f"../processed_images/{session_id}/{image_id}.jpg"
 
+    image_path = f"../processed_images/{session_id}/{image_id}.jpg"
     date = datetime.strptime(date_string, "%Y-%m-%d")
 
-    date = (date.year, date.month, date.day, 0, 0, 0, 0, 0, -1)
-
-    timestamp = time.mktime(date)
+    # Apply EXIF metadata
+    apply_exif_metadata(image_path, date)
+    
+    # Make date tuple for os.utime
+    date_tuple = (date.year, date.month, date.day, 0, 0, 0, 0, 0, -1)
+    timestamp = time.mktime(date_tuple)
     os.utime(image_path, (timestamp, timestamp)) 
+
+    
+def apply_exif_metadata(image_path, date):
+    formatted = datetime(date.year, date.month, date.day, 0, 0, 0)
+
+    with open(image_path, 'rb') as file:
+        img_bytes = file.read()
+
+    img = Image(img_bytes)
+
+    # Update EXIF fields
+    img.set("datetime", formatted)
+    img.set("datetime_original", formatted)
+    img.set("datetime_digitized", formatted)
+
+    # Save image
+    with open(image_path, 'wb') as new_file:
+        new_file.write(img.get_file())
+
+
 
